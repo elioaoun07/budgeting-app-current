427a47fdcfe75eecbd752ceb189144610148696d
eaoundev4c <eaoundev4c@gmail.com>
2025-07-23 20:51:04 +0300

    fix: use CJS export for svelte.config

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+// svelte.config.js
+import vercel from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: vercel()
+  }
+};
+
+export default config;
